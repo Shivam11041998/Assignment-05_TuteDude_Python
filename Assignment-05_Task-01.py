96f1a5b5916af4e7187f8e045d29134dec91b584
Shivam11041998 <shivammalviya0411@gmail.com>
2025-05-27 10:33:15 +0530

    Update Assignment-05_Task-01.py

--- Assignment-05_Task-01.py
+++ Assignment-05_Task-01.py
@@ -1,7 +1,7 @@
 a = {"Shivam": 20, "Rahul": 21, "Raj": 22, "Alice":85, "Bob": 10}
 b = input("Enter the student's name: ")
 if b in a:
-    print(a[b])
+    print(b, " marks's: ",a[b])
 else:
     print("Student not found.")
 
